807184d3b5811556f7ebb6fd3242a472f72fa747
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2018-03-18 18:18:50 -0300

    error handling

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,10 +2,17 @@ import { Component, OnInit, Input, Inject } from '@angular/core';
 import { Hero } from '../model/hero';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
 import { HeroService } from '../hero/hero.service';
 import { HeroServiceInjection } from '../app.injectables';
 import { Observable } from 'rxjs/Observable';
 import { Location } from '@angular/common';
+import { tap } from 'rxjs/operators/tap';
+import { catchError } from 'rxjs/operators/catchError';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/never';
+import { MessageService } from '../message/message.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -17,6 +24,7 @@ export class HeroDetailComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
     private location: Location,
+    private messageService: MessageService,
     @Inject(HeroServiceInjection) private heroService: HeroService) { }
 
   ngOnInit() {
@@ -31,6 +39,12 @@ export class HeroDetailComponent implements OnInit {
     this.route.paramMap.switchMap((paramMap: ParamMap) => {
       const id: number = parseInt(paramMap.get('id'), 10);
       return this.heroService.byId(id);
+    })
+    // using both pipeable operator (tap) and catch
+    .do(hero => this.messageService.add(`Fetched ${hero.name} information`))
+    .catch((error, obs) => {
+      this.messageService.add('Failed to fetch hero details');
+      return Observable.never<Hero>();
     })
     .subscribe((hero) => this.hero = hero);
   }
